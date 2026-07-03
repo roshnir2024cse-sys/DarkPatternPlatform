88d9a8b03379a4f9c88a2a04e533060555238b7c
ROSHNI R <roshni.r.2024.cse@rajalakshmi.edu.in>
2026-07-03 23:43:26 +0530

    Update production API URL

--- frontend-react/src/services/api.js
+++ frontend-react/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://darkpattern-api.onrender.com",
+  baseURL: "https://darkpattern-api-zrwy.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
